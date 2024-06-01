d60252688c9d0e0c3057ca94f22ae8f5e4b8e2e1
Dharmil Shah <shahdharmil01@gmail.com>
2024-06-01 23:54:30 +0530

    cors handling

--- server/index.js
+++ server/index.js
@@ -21,7 +21,12 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+	cors({
+		origin:"https://study-adda-tech.vercel.app",
+		credentials:true,
+	})
+)
 
 app.use(
 	fileUpload({
